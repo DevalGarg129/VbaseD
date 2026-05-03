f25a215fe4fe1238c30eda3928f72c193ab16949
devalgarg129 <devalgarg978@gmail.com>
2026-05-03 11:48:47 +0530

    updating api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "https://vbased.onrender.com/api";
+const baseURL = import.meta.env.VITE_API_URL;
+console.log("API base URL:", baseURL);
 
 const API = axios.create({ baseURL });
 
