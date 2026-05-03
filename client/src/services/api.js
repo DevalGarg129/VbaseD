f2d51d7929c3e3cce40524b84e2cafc0d1612c1f
devalgarg129 <devalgarg978@gmail.com>
2026-05-03 12:15:24 +0530

    resolving issue

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,11 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_API_URL;
 console.log("API base URL:", baseURL);
 
-const API = axios.create({ baseURL });
+const API = axios.create({ 
+  baseURL,
+  withCredentials: true
+});
+console.log("API:", API.defaults.baseURL);
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
